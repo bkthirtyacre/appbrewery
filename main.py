3fd2482fcb238931e0e5b7e5b27c984bd6f906b8
bkthirtyacre <76793810+bkthirtyacre@users.noreply.github.com>
2020-12-30 17:00:39 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#Day 1 Notes
+#print('Day 1 - Python Print Function')
+#print("The function is declared like this:")
+#print("print('what to print')")
+#print('Day 1 - String Manipulation')
+#print('String concatenation is done with the "+" sign.') 
+#print('e.g. print("Hello" + "world")')
+#print("New lines are created with a backslash and n.")
+#Day 1 - Obtain User Info
+#print("Hello " + input("What is your name?"))
+#Day 1 Project- Name Generator
+#1. Create a greeting for your program.
+print("Welcome to your band name generator.")
+#2. Ask the user for the city that they grew up in.
+city=input("What city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet=input("What is the name of one of your pets?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
